f6306b3874f7a82a2ac9202c50da80c397d11217
Scooom <me@scooom.xyz>
2026-04-27 17:46:22 -0500

    fix unpadded dates

--- patches/node/export-fix.js
+++ patches/node/export-fix.js
@@ -59,7 +59,7 @@ const REPLACEMENT = `${i}${encryptLine}
 ${i}const cap = (window as any).Capacitor;
 ${i}if (cap?.isNativePlatform?.()) {
 ${i2}const stampC = new Date();
-${i2}const stamp = stampC.getUTCFullYear() + '_' + (stampC.getUTCMonth() + 1) + '_' +   stampC.getUTCDate() + '-' + stampC.getUTCHours() + '_' +   stampC.getUTCMinutes() + '_' + stampC.getUTCSeconds();
+${i2}const stamp = stampC.getUTCFullYear().toString().padStart(2,'0') + '_' + (stampC.getUTCMonth() + 1).toString().padStart(2,'0') + '_' +   stampC.getUTCDate().toString().padStart(2,'0') + '-' + stampC.getUTCHours().toString().padStart(2,'0') + '_' +   stampC.getUTCMinutes().toString().padStart(2,'0') + '_' + stampC.getUTCSeconds().toString().padStart(2,'0');
 ${i2}const fileName = \`\${dataKey}-\${stamp}.prsv\`;
 ${i2}const base64 = btoa(unescape(encodeURIComponent(${encryptVar}.toString())));
 ${i2}const platform = cap.getPlatform?.() ?? "ios";
